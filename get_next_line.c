593f71389ef2a8db2986ba4c63027cdd75423390
Jun Hong <junh161108@gmail.com>
2026-09-14 15:20:58 +0800

    Revert "."
    This reverts commit 12176b42147cd155c6231f1d6de08c1e78da55b8.

--- get_next_line.c
+++ get_next_line.c
@@ -6,85 +6,19 @@
 /*   By: juho <juho@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/08 15:18:51 by juho              #+#    #+#             */
-/*   Updated: 2026/09/14 15:06:43 by juho             ###   ########.fr       */
+/*   Updated: 2026/09/10 22:37:27 by juho             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char	*extract_line(char *stash)
+char *get_next_line(int fd)
 {
-	size_t	index;
+	char *buffer;
 
-	if (!stash || !stash[0])
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	index = 0;
-	while (stash[index] != '\0' && stash[index] != '\n')
-		index++;
-	return (ft_substr(stash, 0, index + (stash[index] == '\n')));
-}
+	buffer = malloc(BUFFER_SIZE + 1);
 
-static char	*update_stash(char *stash)
-{
-	size_t	index;
-	char	*new_stash;
-
-	if (!stash)
-		return (NULL);
-	index = 0;
-	while (stash[index] != '\0' && stash[index] != '\n')
-		index++;
-	if (stash[index] == '\0')
-	{
-		free(stash);
-		return (NULL);
-	}
-	new_stash = ft_substr(stash, index + 1, ft_strlen(stash) - index - 1);
-	free(stash);
-	return (new_stash);
-}
-
-char	*get_next_line(int fd)
-{
-	static char	*stash;
-	char		buffer[BUFFER_SIZE + 1];
-	ssize_t		bytes_read;
-	char		*line;
-
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
-	{
-		free(stash);
-		stash = NULL;
-		return (NULL);
-	}
-	bytes_read = 1;
-	while (!ft_strchr(stash, '\n') && bytes_read > 0)
-	{
-		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		if (bytes_read < 0)
-		{
-			free(stash);
-			stash = NULL;
-			return (NULL);
-		}
-		buffer[bytes_read] = '\0';
-		stash = ft_strjoin(stash, buffer);
-		if (!stash)
-			return (NULL);
-	}
-	if (!stash || !stash[0])
-	{
-		free(stash);
-		stash = NULL;
-		return (NULL);
-	}
-	line = extract_line(stash);
-	if (!line)
-	{
-		free(stash);
-		stash = NULL;
-		return (NULL);
-	}
-	stash = update_stash(stash);
-	return (line);
+	read (fd, buffer, BUFFER_SIZE);
 }
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,15 +6,15 @@
 /*   By: juho <juho@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/09 18:39:34 by juho              #+#    #+#             */
-/*   Updated: 2026/09/14 15:06:44 by juho             ###   ########.fr       */
+/*   Updated: 2026/09/09 18:39:41 by juho             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef GET_NEXT_LINE_BONUS_H
-# define GET_NEXT_LINE_BONUS_H
+# ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-# include "get_next_line.h"
+char *get_next_line(int fd);
 
-# define MAX_FD 1024
+
 
 # endif
